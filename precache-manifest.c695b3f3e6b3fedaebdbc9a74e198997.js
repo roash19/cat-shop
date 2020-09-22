f18fb8172f06c43814556b370e9b7e0eb25d7d59
Andrey Kozlov <andreykozlov03@yandex.ru>
2020-09-22 23:34:32 +0300

    Updates

---
+++ precache-manifest.c695b3f3e6b3fedaebdbc9a74e198997.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473c6ffb67e250096a1dbfb1b571c9cd",
+    "url": "/cat-shop/index.html"
+  },
+  {
+    "revision": "1947a24465d085145326",
+    "url": "/cat-shop/static/css/main.6b9aee39.chunk.css"
+  },
+  {
+    "revision": "d06fb63dc175578c470e",
+    "url": "/cat-shop/static/js/2.e804ac76.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cat-shop/static/js/2.e804ac76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1947a24465d085145326",
+    "url": "/cat-shop/static/js/main.a385ddaf.chunk.js"
+  },
+  {
+    "revision": "20825daa3fdd51e0bc78",
+    "url": "/cat-shop/static/js/runtime-main.6bc25339.js"
+  },
+  {
+    "revision": "bafc065231d3b9c0cc14874cc5bc0efb",
+    "url": "/cat-shop/static/media/Exo20-Thin.bafc0652.woff"
+  },
+  {
+    "revision": "f69bab65d1b3b547f0250b6cbaf8e1e6",
+    "url": "/cat-shop/static/media/Exo20-Thin.f69bab65.woff2"
+  },
+  {
+    "revision": "6b168ab273d81ae87f07a0b108046d3c",
+    "url": "/cat-shop/static/media/TrebuchetMS-Bold.6b168ab2.woff2"
+  },
+  {
+    "revision": "f5f856b27ebaa0dcc1f14e04f41f55be",
+    "url": "/cat-shop/static/media/TrebuchetMS-Bold.f5f856b2.woff"
+  },
+  {
+    "revision": "14b16f9261d85a0feec93dd83d499e39",
+    "url": "/cat-shop/static/media/TrebuchetMS.14b16f92.woff2"
+  },
+  {
+    "revision": "67bd4472edbde47eea7cf62242fe9583",
+    "url": "/cat-shop/static/media/TrebuchetMS.67bd4472.woff"
+  },
+  {
+    "revision": "24f7dfdc7ad0bbdc4bbac7aa3e630ca4",
+    "url": "/cat-shop/static/media/bg.24f7dfdc.png"
+  },
+  {
+    "revision": "4766b0a286dd6f1c2d6ea007edb45ed0",
+    "url": "/cat-shop/static/media/cat.4766b0a2.png"
+  }
+]);
